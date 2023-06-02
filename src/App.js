4fde4162e7b2eae452a88f318bf30fc2e5acb04b
Benrat17 <benratliff23@gmail.com>
2023-06-02 01:14:52 -0500

    Slight updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './components/header/header';
+import List from './components/list/list';
+import Map from './components/map/map';
+import Button from '@material-ui/core/Button';
+import Placedetails from './components/placedetails/placedetails';
+import { getPlacesData } from './api'
+
+import { CssBaseline, Grid } from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [places, setPlaces] = useState([]);
+
+    const [coords, setCoords] = useState({});
+    const [bounds, setBounds] = useState({});
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude } }) => {
+            setCoords({ lat: latitude, lng: longitude});
+        });
+    }, []);
+
+    function getPlacesButton() {
+        getPlacesData(bounds.sw, bounds.ne)
+            .then((data) => {
+                console.log(data);
+                setPlaces(data);
+            })
+    , [coords, bounds];
+    }
+
+    // useEffect(() => {
+    //     getPlacesData(bounds.sw, bounds.ne)
+    //         .then((data) => {
+    //             console.log(data);
+    //             setPlaces(data);
+    //         })
+    // }, [coords, bounds]);
+
+    return (
+        <>
+            <CssBaseline/>
+            <Header/>
+            <Grid container spacing={3} style={{ width: '100%'}}>
+                <Grid item xs={12} md={4}>
+                    <List places={places}/>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Map
+                        setCoords={setCoords}
+                        setBounds={setBounds}
+                        coords={coords}
+                        places={places}
+                    />
+                    <Button onClick={getPlacesButton()} >Get Restaurants</Button>
+                </Grid>
+            </Grid>
+        </>
+    )
 }
 
 export default App;
